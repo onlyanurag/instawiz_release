98d1b24240db30f02cd89638b219b54425e2755d
onlyanurag <maanshichandela2007@gmail.com>
2025-03-25 21:46:20 +0530

    Update upload_to_telegram.sh

--- scripts/upload_to_telegram.sh
+++ scripts/upload_to_telegram.sh
@@ -2,8 +2,8 @@
 
 # Upload the new APK to Telegram using a bot
 # Replace with your bot token and chat ID
-BOT_TOKEN="YOUR_BOT_TOKEN"
-CHAT_ID="YOUR_CHAT_ID"
+BOT_TOKEN="7537145173:AAEMoB50_cxGnM-Ing_rRCAC7IFy0xfgwzU"
+CHAT_ID="-1002552321923"
 APK_PATH="app/build/outputs/apk/release/app-release.apk"
 
 curl -F document=@"$APK_PATH" "https://api.telegram.org/bot${BOT_TOKEN}/sendDocument?chat_id=${CHAT_ID}&caption=New Instagram alpha release"
